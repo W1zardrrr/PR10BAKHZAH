9162836e8e8d94e80b8df48c0ca848628dcd98c9
mobil <misha139misha@mail.ru>
2024-05-29 15:53:04 +0700

    commit

---
+++ app/src/main/java/com/example/a10_22101/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.a10_22101;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Draw2D draw2D = new Draw2D(this);
+        setContentView(draw2D);
+
+
+    }
+}
